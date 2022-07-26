832edafd5a053aab1889bf2364b1d5a84f721537
noam meir <noamariel8@gmail.com>
2022-07-26 14:29:33 +0300

    cart component

--- src/MainComponents/Cart.js
+++ src/MainComponents/Cart.js
@@ -1,44 +1,39 @@
 import React from 'react'
+import CartItem from '../ToolsComponents/CartItem'
 
 const Cart = () => {
+    const orderDetails = [{
+        src: 'https://png.pngtree.com/thumb_back/fh260/background/20200731/pngtree-blue-carbon-background-with-sport-style-and-golden-light-image_371487.jpg',
+        name: 'background',
+        color: 'blue',
+        quantity: 3,
+        price: 2.99,
+        id: 2
+    },{
+        src: 'https://static.vecteezy.com/system/resources/previews/001/849/553/original/modern-gold-background-free-vector.jpg',
+        name: 'background2',
+        color: 'red',
+        quantity: 2,
+        price: 6.99,
+        id: 4
+    }]
   return (
     <div className='cart'>
         <div class='Header'>
             <h3 className='Heading'>Shopping Cart</h3>
             <h5 className='Action'>Remove all</h5>
         </div>
-        <div class='Cart-Items'>
-            <div class='image-box'>
-                <img src='https://wallpapersflix.com/wp-content/uploads/2021/06/Gaming-Background.jpg' style={{ height:'120px' }} />
-            </div>
-            <div class='about'>
-                <h2 class='title'>Apple Juice</h2>
-                <h3 class='subtitle'>250ml</h3>
-            </div>
-            <div class='counter'> 
-                <div class='btn'>+</div>
-                <div class='count'>2</div>
-                <div class='btn'>-</div>
-            </div>
-            <div class='prices'>
-                <div class='amount'>$2.99</div>
-                <div class='save'>
-                    <u>Save for later</u>
-                </div>
-            <div class='remove'>
-                <u>Remove</u></div>
-            </div>
-        </div>
+        {orderDetails.map(order => <CartItem key={order.id} name={order.name} color={order.color} src={order.src} quantity={order.quantity} price={order.price}></CartItem>)}
         <div class='checkout'>
- <div class='total'>
- <div>
- <div class='Subtotal'>Sub-Total</div>
- <div class='items'>2 items</div>
- </div>
- <div class='total-amount'>$6.18</div>
- </div>
- <button class='button'>Checkout</button>
- </div>
+            <div class='total'>
+                <div>
+                    <div class='Subtotal'>Sub-Total</div>
+                    <div class='items'>2 items</div>
+                </div>
+                <div class='total-amount'>$68.18</div>
+            </div>
+            <button class='button'>Checkout</button>
+        </div>
     </div>
   )
 }
