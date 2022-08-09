7ec39b67f41878cc5a0831854cdb7f240315d20a
noam meir <noamariel8@gmail.com>
2022-08-09 16:30:09 +0300

    added server

--- src/MainComponents/Products.js
+++ src/MainComponents/Products.js
@@ -1,49 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductCard from '../ToolsComponents/ProductCard'
+import { getProducts } from '../api/dal.js'
 
 const Products = () => {
-    const products = [{
-        id: 1,
-        name: 'magevet',
-        description: 'very good magevet',
-        price: 199.99,
-        discount: 0,
-        inStock: 0,
-        img: 'https://i.scdn.co/image/ab67616d0000b2736a51ada54c6090b7138b35ca'
-    },
-    {
-        id: 2,
-        name: 'talit',
-        description: 'very good talit',
-        price: 19.99,
-        discount: 12,
-        inStock: 8,
-        img: 'https://cdn11.bigcommerce.com/s-5qq2dzpnw5/images/stencil/1280x1280/products/1676/43591/3831aab0-aa46-454e-bd03-4f85bc66c6bc__00408.1654095290.jpg?c=2'
-    },{
-        id: 3,
-        name: 'KIPA',
-        description: 'very good KIPA',
-        price: 39.99,
-        discount: 0,
-        inStock: 9,
-        img: 'https://m.media-amazon.com/images/I/81NQEAaAjpL._AC_UL1500_.jpg'
-    },{
-        id: 4,
-        name: 'KIPA',
-        description: 'very good KIPA',
-        price: 39.99,
-        discount: 0,
-        inStock: 9,
-        img: 'https://m.media-amazon.com/images/I/81NQEAaAjpL._AC_UL1500_.jpg'
-    },{
-        id: 5,
-        name: 'KIPA',
-        description: 'very bad KIPA',
-        price: 39.99,
-        discount: 0,
-        inStock: 9,
-        img: 'https://m.media-amazon.com/images/I/81NQEAaAjpL._AC_UL1500_.jpg'
-    }]
+    const [products, setProducts] = useState([])
+    useEffect(()=>{
+        async function getData(){
+            setProducts(await getProducts())
+        }
+        getData()
+    },[])
     const categories = [{id:1,name:'category1'}, {id:2,name:'category2'}, {id:3,name:'category3'}, {id:4,name:'category4'}]
   return (
     <>
@@ -68,7 +34,7 @@ const Products = () => {
             <button>SORT</button>
         </div>
         <div className="products">
-            {products.map(product => <ProductCard key={product.id} name={product.name} description={product.description} price={product.price} discount={product.discount} inStock={product.inStock} img={product.img} id={product.id}></ProductCard>)}
+            {products.map(product => <ProductCard key={product.productid} name={product.productName} description={product.Description.slice(0,10)+'...'} price={product.unitPrice} discount={product.discount==null?0:product.discount} inStock={product.unitInStock} img={product.img} id={product.productid}></ProductCard>)}
         </div>
      
     </div>
